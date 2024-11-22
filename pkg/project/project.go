a030f69626f3be18ce8c5522339c4675a25b597c
Doug Thompson <s-git@dougthompson.co.uk>
2024-11-22 17:23:42 +0000

    Implement currently-horrible export config parser

--- pkg/project/project.go
+++ pkg/project/project.go
@@ -105,12 +105,149 @@ func Load(path string, ignoreStream bool) (p *Project, errs []error) {
 		}
 	}
 
+	// Load export config (note we load Godot's export presets later).
+	// Using a closure here so we can easily short-circuit.
+	(func() {
+		v, ok := pop("export")
+		if !ok {
+			// Nothing to do.
+			return
+		}
+
+		ex, ok := v.(map[string]any)
+		if !ok {
+			pushErrorf("'export': expected table, got %T", v)
+			return
+		}
+
+		for k, v := range ex {
+			// If this is a table, it's an export variant.
+			vt, ok := v.(map[string]any)
+			if ok {
+				variant := Variant{}
+
+				pop := func(key string) (v any, ok bool) {
+					v, ok = vt[key]
+					delete(t, key)
+					return
+				}
+
+				vv, ok := pop("only")
+				if ok {
+					only, ok := vv.([]string)
+					if !ok {
+						pushErrorf(
+							"'export.%s.only': expected string array, got %T",
+							k, vv,
+						)
+					} else {
+						variant.Only = only
+					}
+				}
+
+				vv, ok = pop("volumes")
+				if ok {
+					volumes, ok := vv.(map[string]string)
+					if !ok {
+						pushErrorf(
+							"'export.%s.volumes': expected string-string table, got %T",
+							k, vv,
+						)
+					} else {
+						variant.Volumes = volumes
+					}
+				}
+
+				vv, ok = pop("scripts")
+				if ok {
+					scripts, ok := vv.(map[string]any)
+					if !ok {
+						pushErrorf(
+							"'export.%s.scripts': expected table, got %T",
+							k, vv,
+						)
+					} else {
+						vvv, ok := scripts["pre"]
+						if ok {
+							pre, ok := vvv.(string)
+							if !ok {
+								pushErrorf(
+									"'export.%s.scripts.pre': expected string, got %T",
+									k, vvv,
+								)
+							} else {
+								variant.Scripts.Pre = pre
+							}
+						}
+
+						vvv, ok = scripts["post"]
+						if ok {
+							post, ok := vvv.(string)
+							if !ok {
+								pushErrorf(
+									"'export.%s.scripts.post': expected string, got %T",
+									k, vvv,
+								)
+							} else {
+								variant.Scripts.Post = post
+							}
+						}
+
+						delete(scripts, "pre")
+						delete(scripts, "post")
+						if len(scripts) != 0 {
+							pushErrorf("'export.%s.scripts: unknown keys", k)
+						}
+					}
+				}
+
+				vv, ok = pop("elective")
+				if ok {
+					elective, ok := vv.(bool)
+					if !ok {
+						pushErrorf(
+							"'export.%s.elective': expected bool, got %T",
+							k, vv,
+						)
+					} else {
+						variant.Elective = elective
+					}
+				}
+
+				p.Export.Variants[k] = variant
+				continue
+			}
+
+			switch k {
+			case "only":
+				only, ok := v.([]string)
+				if !ok {
+					pushErrorf(
+						"'export.only': expected string array, got %T",
+						v,
+					)
+				} else {
+					p.Export.Only = only
+				}
+
+			case "dist":
+				dist, ok := v.(string)
+				if !ok {
+					pushErrorf("'export.dist': expected string, got %T", v)
+				} else {
+					p.Export.Dist = dist
+				}
+
+			default:
+				pushErrorf("'export.%s': expected table, got %T", v)
+			}
+		}
+	})()
+
 	// Error on remaining keys.
 	for k := range t {
 		pushErrorf("'%s': unknown key", k)
 	}
-
-	// TODO: read export config.
 
 	// ---
 	// Now we start reading Godot config from src.
